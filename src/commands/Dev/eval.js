32015a804c0b2beb6342cead94733a5f8a635d65
JarnoPwr <jarnostoffer@gmail.com>
2021-04-02 19:04:52 +0200

    Add eval command

---
+++ src/commands/Dev/eval.js
@@ -0,0 +1,56 @@
+module.exports = {
+    info: {
+        name: "eval",
+        aliases: ["e"],
+        usage: "eval <code>",
+        examples: ["eval 1+1"],
+        description: "Runs code.",
+        category: "Dev",
+        info: null,
+        options: []
+    },
+    perms: {
+        type: "dev",
+        self: []
+    },
+    opts: {
+        guildOnly: false,
+        devOnly: true,
+        noArgsHelp: true,
+        disabled: false
+    },
+
+    run: async (bot, message, args) => {
+
+        // Clean the eval from any confidential data
+        const clean = (text) => {
+            if (typeof (text) === "string") {
+                return text
+                    .replace(/`/g, `\`${String.fromCharCode(8203)}`)
+                    .replace(/@/g, `@${String.fromCharCode(8203)}`)
+                    .replace(bot.config.general.token, "Access Denied");
+            }
+            
+            return text;
+        };
+
+        try {
+            // Get the code to eval
+            const code = args.join(" ");
+            // Eval the code
+            let evaled = await eval(code);
+
+            // Inspect the code if it didn't return a string
+            if (typeof (evaled) !== "string") {
+                evaled = require("util").inspect(evaled);
+            }
+
+            // Return the response
+            message.channel.send(clean(evaled), { code: "xl", split: { maxLength: "1800" } });
+        } catch (err) {
+            // Return an error if something went wrong
+            message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+        }
+
+    }
+};
