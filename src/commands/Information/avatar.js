f8bb7c01c1a98a7bc8762c992e143a6ac6cc1807
JarnoPwr <jarnostoffer@gmail.com>
2021-04-06 18:10:22 +0200

    Add avatar command

---
+++ src/commands/Information/avatar.js
@@ -0,0 +1,46 @@
+const { MessageEmbed } = require("discord.js");
+const { getMember, getUser } = require("../../modules/functions/getters");
+
+module.exports = {
+    info: {
+        name: "avatar",
+        aliases: ["av", "a"],
+        usage: "avatar [user]",
+        examples: ["avatar Jarno"],
+        description: "Display the avatar of a user or yourself.",
+        category: "Information",
+        info: null,
+        options: []
+    },
+    perms: {
+        permission: ["@everyone"],
+        type: "role",
+        self: ["EMBED_LINKS"]
+    },
+    opts: {
+        guildOnly: false,
+        devOnly: false,
+        noArgsHelp: false,
+        disabled: false
+    },
+
+    run: async (bot, message, args) => {
+
+        // Get the user
+        const target = await getMember(message, args.join(" "), true)?.user || await getUser(bot, message, args.join(" "), true);
+
+        // If a invalid user was specified return an error
+        if (!target)
+            return message.error("You didn't specify a valid user!");
+
+        // Build the embed
+        const embed = new MessageEmbed()
+            .setColor(message.member?.displayColor ? message.member.displayHexColor : "#ABCDEF")
+            .setAuthor(`Avatar for ${target.tag}`, target.displayAvatarURL({ format: "png", dynamic: true }))
+            .setImage(target.displayAvatarURL({ format: "png", dynamic: true, size: 1024}));
+
+        // Send the embed
+        message.channel.send(embed);
+
+    }
+};
