fcdf8178b68f6b94def5926f89f996e275fe3544
JarnoPwr <jarnostoffer@gmail.com>
2021-04-09 18:53:12 +0200

    Add 8ball command

---
+++ src/commands/Fun/8ball.js
@@ -0,0 +1,57 @@
+module.exports = {
+    info: {
+        name: "8ball",
+        aliases: ["8"],
+        usage: "8ball <question>",
+        examples: ["8ball is today a good day?"],
+        description: "Ask the 8ball questions.",
+        category: "Fun",
+        info: "A list of possible answers can be found [here](https://en.wikipedia.org/wiki/Magic_8-Ball#Possible_answers).",
+        options: []
+    },
+    perms: {
+        permission: ["@everyone"],
+        type: "role",
+        self: []
+    },
+    opts: {
+        guildOnly: false,
+        devOnly: false,
+        noArgsHelp: true,
+        disabled: false
+    },
+
+    run: async (bot, message) => {
+
+        // All possible 8ball answers (taken from https://en.wikipedia.org/wiki/Magic_8-Ball#Possible_answers)
+        const answers = [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes, definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+        ];
+
+        // Get a random answer
+        const result = Math.floor((Math.random() * answers.length));
+
+        // Send the answer
+        message.channel.send(`🎱 ${answers[result]}`);
+
+    }
+};
