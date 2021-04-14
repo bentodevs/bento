5ad234ba41566d4c28821cc28ec7ec49e61a86db
JarnoPwr <jarnostoffer@gmail.com>
2021-04-14 15:11:02 +0200

    Add choose command

---
+++ src/commands/Fun/choose.js
@@ -0,0 +1,40 @@
+module.exports = {
+    info: {
+        name: "choose",
+        aliases: ["pick", "decide"],
+        usage: "choose <options>",
+        examples: ["choose 1 | 2 | 3", "choose heads | tails"],
+        description: "Randomly chooses an option for you.",
+        category: "Fun",
+        info: null,
+        options: []
+    },
+    perms: {
+        permission: ["@everyone"],
+        type: "role",
+        self: []
+    },
+    opts: {
+        guildOnly: false,
+        devOnly: false,
+        noArgsHelp: true,
+        disabled: false
+    },
+
+    run: async (bot, message, args) => {
+
+        // Split the options into an array
+        const options = args.join(" ").split("|");
+
+        // If no options were specified or all options are the same return an error
+        if (options.every((val, i, arr) => val.trim() === arr[0].trim()))
+            return message.error("You didn't really specify an options there?");
+
+        // Choose a random option
+        const choice = Math.floor((Math.random() * options.length));
+
+        // Send the chosen option
+        message.channel.send(`🤔 I choose ${options[choice].trim()}`);
+
+    }
+};
