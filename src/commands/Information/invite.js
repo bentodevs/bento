06cde4af34e26a7ba68ae22749d30e8aa6604727
JarnoPwr <jarnostoffer@gmail.com>
2021-04-14 15:16:09 +0200

    Add invite command

---
+++ src/commands/Information/invite.js
@@ -0,0 +1,39 @@
+module.exports = {
+    info: {
+        name: "invite",
+        aliases: ["createinvite", "inv"],
+        usage: "",
+        examples: [],
+        description: "Creates a permanent guild invite.",
+        category: "Information",
+        info: null,
+        options: []
+    },
+    perms: {
+        permission: "CREATE_INSTANT_INVITE",
+        type: "discord",
+        self: ["CREATE_INSTANT_INVITE"]
+    },
+    opts: {
+        guildOnly: true,
+        devOnly: false,
+        noArgsHelp: false,
+        disabled: false
+    },
+
+    run: async (bot, message) => {
+
+        if (message.guild.vanityURLCode) {
+            // If the guild has a vanity URL send it
+            message.channel.send(`https://discord.gg/${message.guild.vanityURLCode}`);
+        } else {
+            // Create a permanent invite and send it
+            message.channel.createInvite({ maxAge: 0 }).then(invite => {
+                message.channel.send(invite.toString());
+            }).catch(err => {
+                message.error(`Something went wrong while creating the invite: \`${err.message}\``);
+            });
+        }
+
+    }
+};
