ea05393a83016b5bcb4781f1fa0e590da884042a
WaitroseDev <hello@behn.cc>
2021-04-22 16:06:55 +0100

    Update logger

--- src/modules/functions/logger.js
+++ src/modules/functions/logger.js
@@ -8,21 +8,21 @@ exports.log = (content, type = "log") => {
             return console.log(`${timestamp} ${chalk.bgBlue(type.toUpperCase())} ${content} `);
         }
         case "warn": {
-            return console.log(`${timestamp} ${chalk.black.bgYellow(type.toUpperCase())} ${content} `);
+            return console.warn(`${timestamp} ${chalk.black.bgYellow(type.toUpperCase())} ${content} `);
         }
         case "error": {
-            return console.log(`${timestamp} ${chalk.bgRed(type.toUpperCase())} ${content} `);
+            return console.error(`${timestamp} ${chalk.bgRed(type.toUpperCase())} ${content} `);
         }
         case "debug": {
-            return console.log(`${timestamp} ${chalk.green(type.toUpperCase())} ${content} `);
+            return console.debug(`${timestamp} ${chalk.green(type.toUpperCase())} ${content} `);
         }
         case "cmd": {
-            return console.log(`${timestamp} ${chalk.black.bgWhite(type.toUpperCase())} ${content}`);
+            return console.info(`${timestamp} ${chalk.black.bgWhite(type.toUpperCase())} ${content}`);
         }
         case "ready": {
-            return console.log(`${timestamp} ${chalk.black.bgGreen(type.toUpperCase())} ${content}`);
+            return console.info(`${timestamp} ${chalk.black.bgGreen(type.toUpperCase())} ${content}`);
         }
-        default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd, sql or error.");
+        default: throw new TypeError("Logger type must be either warn, debug, log, ready or cmd");
     }
 };
 
