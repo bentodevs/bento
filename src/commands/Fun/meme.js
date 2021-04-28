3937ca85cd685913a8da4e65d8bfc8d0ee39bc56
JarnoPwr <jarnostoffer@gmail.com>
2021-04-28 16:13:44 +0200

    meme command

---
+++ src/commands/Fun/meme.js
@@ -0,0 +1,51 @@
+const { MessageEmbed } = require("discord.js");
+const { getMeme } = require("../../modules/functions/misc");
+
+module.exports = {
+    info: {
+        name: "meme",
+        aliases: [
+            "memes",
+            "dankmemes",
+            "dankmeme"
+        ],
+        usage: "",
+        examples: [],
+        description: "Sends a random meme.",
+        category: "Fun",
+        info: "Memes are taken from the following subreddits: [r/memes](https://www.reddit.com/r/memes), [r/dankmemes](https://www.reddit.com/r/dankmemes), [r/wholesomememes](https://www.reddit.com/r/wholesomememes), [r/BikiniBottomTwitter](https://www.reddit.com/r/BikiniBottomTwitter), [r/funny](https://www.reddit.com/r/funny)",
+        options: []
+    },
+    perms: {
+        permission: ["@everyone"],
+        type: "role",
+        self: [
+            "EMBED_LINKS"
+        ]
+    },
+    opts: {
+        guildOnly: false,
+        devOnly: false,
+        noArgsHelp: false,
+        disabled: false
+    },
+
+    run: async (bot, message, args) => {
+
+        // Send a status message & get a random meme
+        const msg = await message.loading("Requesting meme..."),
+        meme = await getMeme();
+
+        // Build the embed
+        const embed = new MessageEmbed()
+            .setColor(message.member?.displayColor ?? bot.config.general.embedColor)
+            .setImage(meme.data.url)
+            .setDescription(`[${meme.data.title}](https://www.reddit.com/${meme.data.permalink})`)
+            .setFooter(meme.data.subreddit_name_prefixed, bot.user.displayAvatarURL({ format: "png", dynamic: true }));
+
+        // Delete the status message & send the embed
+        msg.delete().catch(() => {});
+        message.channel.send(embed);
+
+    }
+};
--- src/modules/functions/misc.js
+++ src/modules/functions/misc.js
@@ -1,5 +1,20 @@
 const { default: fetch } = require("node-fetch");
 
+/**
+ * Get a definition from the urbandictionary api
+ * 
+ * @param {String} query
+ *  
+ * @returns {Promise<Object>} definition
+ * 
+ * @example
+ * 
+ * urban("lol").then(data => {
+ *      console.log(data);
+ * }).catch(err => {
+ *      console.error(err);
+ * })
+ */
 exports.urban = (query) => {
     return new Promise((resolve, reject) => {
         // Define the endpoint
@@ -29,6 +44,60 @@ exports.urban = (query) => {
 
             // Return the result
             return resolve(result);
+        }).catch(err => {
+            // Log and reject the error
+            console.error(err);
+            reject(err);
+        });
+    });
+};
+
+/**
+ * Gets a random meme from reddit
+ * 
+ * @returns {Promise<Object>} Reddit Post
+ * 
+ * @example
+ * 
+ * getMeme().then(data => {
+ *      console.log(data);
+ * }).catch(err => {
+ *      console.error(err);
+ * })
+ */
+exports.getMeme = () => {
+    return new Promise((resolve, reject) => {
+        // Define the endpoint
+        const endpoint = "https://www.reddit.com/";
+        
+        // Define all the subreddits
+        const subs = [
+            "memes",
+            "dankmemes",
+            "wholesomememes",
+            "BikiniBottomTwitter",
+            "funny"
+        ];
+    
+        // Get a random subreddit
+        const sub = subs[Math.floor(Math.random() * subs.length)];
+
+        // Fetch the reddit api
+        fetch(`${endpoint}r/${sub}.json?sort=top&t=week&limit=100`, {
+            headers: { "content-type": "application/json" }
+        }).then(res => res.json()).then(json => {
+            // Filter out all the bad posts
+            const filtered = json.data.children.filter(p => !p.data.over_18 && p.data.post_hint !== "hosted:video" && p.data.post_hint !== "link" && p.data.post_hint !== "self" && p.data.post_hint);
+
+            // If no memes remain return an error
+            if (!filtered.length)
+                return reject(new Error("No Memes Found!"));
+
+            // Get a random post
+            const post = filtered[Math.floor(Math.random() * filtered.length)];
+
+            // Return the post
+            resolve(post);
         }).catch(err => {
             // Log and reject the error
             console.error(err);
