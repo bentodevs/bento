08ba465cd12fa418801b1a37c9946622d46b88ac
JarnoPwr <jarnostoffer@gmail.com>
2021-04-29 15:13:20 +0200

    Add channelinfo command

---
+++ src/commands/Information/channelinfo.js
@@ -0,0 +1,109 @@
+const { format, formatDistance, formatDuration, intervalToDuration } = require("date-fns");
+const { MessageEmbed } = require("discord.js");
+const { getChannel } = require("../../modules/functions/getters");
+
+module.exports = {
+    info: {
+        name: "channelinfo",
+        aliases: [
+            "cinfo"
+        ],
+        usage: "channelinfo [channel]",
+        examples: [
+            "channelinfo #general",
+            "channelinfo vc-1"
+        ],
+        description: "Show information about a channel.",
+        category: "Information",
+        info: null,
+        options: []
+    },
+    perms: {
+        permission: "MANAGE_CHANNELS",
+        type: "discord",
+        self: ["EMBED_LINKS"]
+    },
+    opts: {
+        guildOnly: true,
+        devOnly: false,
+        noArgsHelp: false,
+        disabled: false
+    },
+
+    run: async (bot, message, args) => {
+
+        // Grab the channel
+        const channel = await getChannel(message, args.join(" "), true);
+
+        // If the user specified a invalid channel return an error!
+        if (!channel)
+            return message.error("You didn't specify a valid channel!");
+
+        // Define formatting for all the channel types
+        const types = {
+            text: {
+                type: "Channel",
+                icon: "https://i.imgur.com/6VyvJWL.png"
+            },
+            voice: {
+                type: "Voice Channel",
+                icon: "https://i.imgur.com/yXE4Yg9.png"
+            },
+            category: {
+                type: "Category",
+                icon: "https://i.imgur.com/Oyl9rvi.png"
+            },
+            news: {
+                type: "News Channel",
+                icon: "https://i.imgur.com/6VyvJWL.png"
+            },
+            store: {
+                type: "Store Channel",
+                icon: "https://i.imgur.com/6VyvJWL.png"
+            }
+        };
+
+        // 1. Format the channel creation time
+        // 2. Format the time since the channel was created
+        const channelCreated = format(channel.createdTimestamp, "PPp"),
+        timeSince = formatDistance(channel.createdTimestamp, Date.now(), { addSuffix: true });
+
+        // Define the desc and lastMessage vars
+        let desc = "",
+        lastMessage;
+
+        // Build the description
+        desc += `**Created:** ${channelCreated} (${timeSince})\n`;
+        desc += `**Position:** ${channel.position}${channel.parent ? " (in category)" : ""} | [Open Channel](https://discord.com/channels/${message.guild.id}/${channel.id})\n`;
+
+        if (channel.children)
+            desc += `**Channels:** ${channel.children.array().join(", ")}\n`;
+        if (channel.parentID)
+            desc += `**Category:** ${message.guild.channels.cache.get(channel.parentID).name}\n`;
+        if (channel.bitrate)
+            desc += `**Bitrate:** ${channel.bitrate}kbps\n`;
+        if (channel.members && channel.type == "voice")
+            desc += `**Users Connected:** ${channel.members ? channel.members.size : 0}\n`;
+        if (channel.topic)
+            desc += `**Topic:** ${channel.topic !== "" ? channel.topic : "None"}\n`;
+        if (channel.nsfw)
+            desc += `**NSFW:** True\n`;
+        if (channel.lastMessageID)
+            lastMessage = await channel.messages.fetch(channel.lastMessageID).catch(() => { });
+        if (channel.lastMessageID && lastMessage)
+            desc += `**Last Activity:** ${formatDistance(lastMessage.createdTimestamp, Date.now(), { addSuffix: true })}`;
+        if (channel.rateLimitPerUser > 0)
+            desc += `\n**Rate Limit:** ${formatDuration(intervalToDuration({ start: 0, end: channel.rateLimitPerUser * 1000 }), { delimiter: ", " })}`;
+
+        // Build the embed
+        const embed = new MessageEmbed()
+            .setAuthor(`${types[channel.type].type}: ${channel.name}`, types[channel.type].icon)
+            .setDescription(desc)
+            .setFooter(`ID: ${channel.id}`)
+            .setColor(message.member.displayColor ? message.member.displayHexColor : "#ABCDEF");
+
+        // Send the embed
+        message.channel.send(embed);
+
+    }
+};
