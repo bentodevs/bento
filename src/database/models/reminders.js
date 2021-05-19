2cc8abb794a196209d3d34c9a3047eb5479884c3
JarnoPwr <jarnostoffer@gmail.com>
2021-05-19 18:44:39 +0200

    Add the reminders model

---
+++ src/database/models/reminders.js
@@ -0,0 +1,6 @@
+const { model, Schema } = require("mongoose");
+
+module.exports = model("reminders", new Schema({
+    _id: String, // The Discord ID of the user
+    reminders: Array // The array of reminders
+}));
