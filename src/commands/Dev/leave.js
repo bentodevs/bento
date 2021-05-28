ce4b6f74b4a4280836bb21fa02be7ca28e9af1ef
JarnoPwr <jarnostoffer@gmail.com>
2021-05-28 18:31:36 +0200

    Add leave command

---
+++ src/commands/Dev/leave.js
@@ -0,0 +1,71 @@
+const { stripIndents } = require("common-tags");
+
+module.exports = {
+    info: {
+        name: "leave",
+        aliases: [
+            "leaveserver"
+        ],
+        usage: "leave <guild id | \"this\">",
+        examples: [
+            "leave this",
+            "leave 839939523231875082"
+        ],
+        description: "Force leaves a discord.",
+        category: "Dev",
+        info: null,
+        options: []
+    },
+    perms: {
+        type: "dev",
+        self: []
+    },
+    opts: {
+        guildOnly: false,
+        devOnly: true,
+        noArgsHelp: false,
+        disabled: false
+    },
+
+    run: async (bot, message, args) => {
+
+        // Get the guild
+        const guild = args[0].toLowerCase() == "this" ? message.guild ?? null : await bot.guilds.fetch(args[0]).catch(() => {});
+
+        // If the guild wasn't found return an error
+        if (!guild)
+            return message.error("You didn't specify a valid guild!");
+
+        // Send a message asking if the user is sure
+        await message.channel.send(stripIndents`Are you sure you want me to leave \`${guild.name} (${guild.id})\`?
+        
+        Type \`y\` or \`yes\` to continue.`);
+
+        // Define the filter and options
+        const filter = m => m.author.id == message.author.id,
+        options = { time: 60000, max: 1, errors: ["time"] };
+
+        // Await a response
+        await message.channel.awaitMessages(filter, options).then(async msgs => {
+            // Get the first response
+            const m = msgs.first();
+
+            // If the user typed "y" or "yes" leave the discord otherwise send the canceled message
+            if (["y", "yes"].includes(m.content.toLowerCase())) {
+                await guild.leave();
+                
+                if (guild.id == message.guild.id) {
+                    message.author.send(`${bot.config.emojis.confirmation} Successfully left \`${guild.name} (${guild.id})\`!`);
+                } else {
+                    message.confirmation(`Successfully left \`${guild.name} (${guild.id})\`!`);
+                }
+            } else {
+                message.confirmation("The command has been canceled.");
+            }
+        }).catch(() => {
+            // Send an error message
+            message.error("The command has been canceled.");
+        });
+
+    }
+};
