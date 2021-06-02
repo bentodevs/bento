fb6183e1b01143f19af053fa5540c5213a8af45c
WaitroseDev <hello@behn.cc>
2021-06-02 14:50:16 +0100

    feat(fun-cmds): ✨ Add iss command
    Fetches info about the International Space Station

---
+++ src/commands/Fun/iss.js
@@ -0,0 +1,59 @@
+const { stripIndents } = require("common-tags");
+const { MessageEmbed } = require("discord.js");
+const { default: fetch } = require("node-fetch");
+
+module.exports = {
+    info: {
+        name: "iss",
+        aliases: ["spacestation"],
+        usage: "",
+        examples: [],
+        description: "Get information about the International Space Station",
+        category: "Fun",
+        info: null,
+        options: []
+    },
+    perms: {
+        permission: ["@everyone"],
+        type: "role",
+        self: ["EMBED_LINKS"]
+    },
+    opts: {
+        guildOnly: false,
+        devOnly: false,
+        premium: false,
+        noArgsHelp: false,
+        disabled: false
+    },
+
+    run: async (bot, message, args) => {
+
+        const iss = await (await fetch("http://api.open-notify.org/iss-now.json")).json(),
+            issPeople = await (await fetch("http://api.open-notify.org/astros.json")).json();
+        
+        const humans = issPeople.people.reduce((a, b) => {
+            // deepcode ignore PrototypePollution: Won't be fixed
+            a[b.craft] = a[b.craft] || [];
+            a[b.craft].push(b.name)
+            return a;
+        }, {});
+
+        const embed = new MessageEmbed()
+            .setAuthor('ISS Info', 'https://cdn.freelogovectors.net/wp-content/uploads/2016/12/nasa-logo.png')
+            .setColor(message.member.displayHexColor ?? bot.config.general.embedColor)
+            .setThumbnail("https://cdn.freelogovectors.net/wp-content/uploads/2016/12/nasa-logo.png")
+            .setDescription(stripIndents`**Current Location:** ${iss.iss_position.latitude}, ${iss.iss_position.longitude},
+            **Current Astronauts:** ${issPeople.number}`)
+            .setFooter("Last updated")
+            .setTimestamp()
+        
+        for (let [craft, ppl] of Object.entries(humans)) {
+            for (let i = 0; i < ppl.length; i++) {
+                ppl[i] = `${i + 1}. ${ppl[i]}`;
+            }
+            embed.addField(`${craft}`, ppl.join('\n'))
+        }
+
+        message.channel.send(embed)
+    }
+};
