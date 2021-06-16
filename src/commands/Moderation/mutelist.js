3d8877ae0186d0761560e600029bc2d55cbe14b6
WaitroseDev <hello@behn.cc>
2021-06-16 10:51:24 +0100

    feat(commands): ✨ Add slash command to mutelist

--- src/commands/Moderation/mutelist.js
+++ src/commands/Moderation/mutelist.js
@@ -25,8 +25,12 @@ module.exports = {
         noArgsHelp: false,
         disabled: false
     },
+    slash: {
+        enabled: true,
+        opts: []
+    },
 
-    run: async (bot, message, args) => {
+    run: async (bot, message) => {
 
         // Find all mutes with the guild ID set as their guild value
         const mutedata = await mutes.find({ guild: message.guild.id });
@@ -51,5 +55,31 @@ module.exports = {
 
         // Send the completed message
         message.channel.send(muteList, { split: { char: "\n" } });
+    },
+
+    run_interaction: async (bot, interaction) => {
+        // Find all mutes with the guild ID set as their guild value
+        const mutedata = await mutes.find({ guild: interaction.guild.id });
+        // Define mute list text
+        let muteList = `🔇 Muted users as of **${format(Date.now(), "PPp")}**\n\n`;
+
+        // If there are no mutes, then return error
+        if (mutedata.length < 1)
+            return interaction.error(`There currently aren't any muted members!`);
+
+        for (const data of mutedata) {
+            // 1. Get the muted user
+            // 2. Get the executor
+            // 3. Add the time muted for to the mute time
+            const mUser = await (bot.users.cache.get(data.mutedUser) || bot.users.fetch(data.mutedUser).catch(() => { })),
+                mBy = await (bot.users.cache.get(data.mutedBy) || bot.users.fetch(data.mutedBy).catch(() => { })),
+                time = data.timeMuted + parseInt(data.muteTime);
+
+            muteList += `${data.caseID ? `**ID:** ${data.caseID} | ` : ""}**User:** \`${mUser.tag}\` (${mUser.id}) | **Muted By:** \`${mBy.tag}\` | **Expires:** ${data.muteTime == "forever" ? "never" : time <= Date.now() ? `<Pending Unmute>` : `in ${formatDistance(Date.now(), time)}`} | **Reason:** ${data.reason}\n`;
+
+        }
+
+        // Send the completed message
+        interaction.reply(muteList, { split: { char: "\n" } });
     }
 };
