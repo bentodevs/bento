999cdfdcee97cdc76a06ebf39e6be01b47182fbe
WaitroseDev <hello@behn.cc>
2021-06-16 11:19:56 +0100

    feat(commands): ✨ Add slash command to reason

--- src/commands/Moderation/reason.js
+++ src/commands/Moderation/reason.js
@@ -23,6 +23,20 @@ module.exports = {
         noArgsHelp: true,
         disabled: false
     },
+    slash: {
+        enabled: true,
+        opts: [{
+            name: "case",
+            type: "INTEGER",
+            description: "The user you wish to mute.",
+            required: true
+        }, {
+            name: "reason",
+            type: "STRING",
+            description: "Nickname you wish to set (Do not enter anything to remove the nickname)",
+            required: true
+        }]
+    },
 
     run: async (bot, message, args) => {
 
@@ -39,5 +53,21 @@ module.exports = {
         await punishments.findOneAndUpdate({ id: args[0], guild: message.guild.id }, { reason: reason })
             .then(() => message.confirmation(`The reason for ${punishment.id} has been updated to ${reason}`))
             .catch((err) => message.error(`I encountered an error whilst updating the reason for ${punishment.id}: \`${err.message}\``));
+    },
+
+    run_interaction: async (bot, interaction) => {
+
+        // Get the member and the nick
+        const caseID = interaction.options.get("case").value,
+        reason = interaction.options.get("reason").value;
+
+        const punishment = await punishments.findOne({ id: caseID, guild: interaction.guild.id });
+
+        if (!punishment)
+            return interaction.error("You must specify a valid case!");
+
+        await punishments.findOneAndUpdate({ id: caseID, guild: interaction.guild.id }, { reason: reason })
+            .then(() => interaction.confirmation(`The reason for Case ID ${punishment.id} has been updated to ${reason}`))
+            .catch((err) => interaction.error(`I encountered an error whilst updating the reason for ${punishment.id}: \`${err.message}\``));
     }
 };
