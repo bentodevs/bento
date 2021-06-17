416849c55f2f6782359d005f0ba4b9c551a14810
Jarno <jarnostoffer@gmail.com>
2021-06-17 16:07:26 +0200

    refactor(events): :art: Add .catch() to prevent errors

--- src/events/message.js
+++ src/events/message.js
@@ -26,7 +26,7 @@ module.exports = async (bot, message) => {
     // If the channel is a command channel delete the message after 15 seconds
     if (message.settings.general.command_channel) {
         if (message.channel.id === message.settings.general.command_channel)
-            setTimeout(() => message.delete(), 15000);
+            setTimeout(() => message.delete().catch(() => {}), 15000);
     }
 
     // Return if the user is a bot
