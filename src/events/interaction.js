a89af177a49213e4489d3108aec373efb9d226a5
Jarno <jarnostoffer@gmail.com>
2021-06-17 15:45:52 +0200

    feat(events): :sparkles: Properly implement the command section of the interaction event

--- src/events/interaction.js
+++ src/events/interaction.js
@@ -1,42 +1,61 @@
 const { stripIndents } = require("common-tags");
 const { MessageEmbed } = require("discord.js");
-const { getSettings } = require("../database/mongo");
+const { getSettings, getPerms } = require("../database/mongo");
+const { checkPerms, checkSelf } = require("../modules/functions/permissions");
 
 module.exports = async (bot, interaction) => {
 
     if (interaction.isMessageComponent() && interaction.componentType == "BUTTON") {
         // Button Code
     } else if (interaction.isCommand()) {
+        // Return if the user is a bot
         if (interaction.user.bot)
             return;
 
-        const settings = interaction.settings = await getSettings(interaction.guild?.id);
+        // Get the settings and permissions
+        const settings = interaction.settings = await getSettings(interaction.guild?.id),
+        permissions = interaction.permissions = await getPerms(bot, interaction.guild?.id);
 
+        // If the member isn't found try to fetch it
         if (interaction.guild && !interaction.member)
-            await interaction.guild.members.fetch(interaction.user);
+            await interaction.guild.members.fetch(interaction.user).catch(() => {});
 
+        // Import the interaction functions
         require("../modules/functions/interactions")(interaction);
 
+        // Get the command
         const cmd = bot.commands.get(interaction.commandName);
 
+        // If the command doesn't exist return an error
         if (!cmd)
-            return interaction.error("The command you ran wasn't found!", { ephemeral: true });
-
+            return interaction.error({ content: "The command you ran wasn't found!", ephemeral: true });
+        // If the command is disabled return an error
         if (cmd.opts.disabled && !bot.config.general.devs.includes(interaction.user.id))
-            return interaction.error("This command is currently disabled!", { ephemeral: true });
-
+            return interaction.error({ content: "This command is currently disabled!", ephemeral: true });
+        // If a guildOnly command is run in dms return an error
         if (cmd.opts.guildOnly && !interaction.guild)
-            return interaction.error("This command is unavailable via private messages. Please run this command in a guild.", { ephemeral: true });
-
+            return interaction.error({ content: "This command is unavailable via private messages. Please run this command in a guild.", ephemeral: true });
+        // If the command or category is disabled return an error
         if (interaction.guild && (settings.general.disabled_commands?.includes(cmd.info.name) || settings.general.disabled_categories?.includes(cmd.info.category)) && !interaction.channel.permissionsFor(interaction.member).has("ADMINISTRATOR") && !bot.config.general.devs.includes(interaction.user.id))
-            return interaction.error("This command (or the category the command is in) is currently disabled!", { ephemeral: true });
+            return interaction.error({ content: "This command (or the category the command is in) is currently disabled!", ephemeral: true });
 
+        // If the bot doesn't have permissions to run the command return
+        if (await checkSelf(interaction, cmd))
+            return;
+        // Check if the user has permissions to run the command
+        if (await checkPerms(bot, interaction, permissions, cmd))
+            return interaction.error({ content: "You don't have permissions to run that command!", ephemeral: true });
+
+        // Try to run the command
         try {
+            // Run the command
             cmd.run_interaction ? await cmd.run_interaction(bot, interaction) : await cmd.run(bot, interaction);
         } catch (err) {
+            // Get the error guild and channel
             const guild = bot.guilds.cache.get(bot.config.general.errors.guild) || await bot.guilds.fetch(bot.config.general.errors.guild).catch(() => {}),
             channel = guild?.channels.cache.get(bot.config.general.errors.channel);
 
+            // Build the embed
             const embed = new MessageEmbed()
                 .setAuthor(`An error occured in ${interaction.guild?.name ?? `${interaction.user.tag}'s dms`}`, "https://i.imgur.com/e7xORGp.png")
                 .setThumbnail("https://i.imgur.com/e7xORGp.png")
@@ -48,11 +67,13 @@ module.exports = async (bot, interaction) => {
                 **Error:**\`\`\`${err.stack}\`\`\``)
                 .setColor("#FF2D00");
 
+            // Send the embed
             channel?.send({ embeds: [embed] });
 
-            interaction.error(stripIndents`An error occurred while running the command: \`${err}\`
+            // Send the error message to the user
+            interaction.error({ content: stripIndents(`An error occurred while running the command: \`${err}\`
             
-            ${bot.config.emojis.url} If this issue persists please report it in our discord: ${bot.config.general.errors.url}`, { ephemeral: true });
+            ${bot.config.emojis.url} If this issue persists please report it in our discord: ${bot.config.general.errors.url}`), ephemeral: true });
         }
     }
 };
