723d0accf88987b71bdd3a8f6b7e1a610b6bce10
WaitroseDev <hello@behn.cc>
2021-06-30 14:44:25 +0100

    perf(events): ⚡️ Create settings on guild join

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,6 +1,12 @@
 const config = require("../config");
+const settings = require("../database/models/settings");
 
 module.exports = async (bot, guild) => {
+
+    // If there are no guild settings, then create them for the guild
+    if (!await settings.findOne({ _id: guild.id })) {
+        await new settings({ _id: guild.id }).save();
+    }
     
     // Get the joins guild and channel
     const g = bot.guilds.cache.get(config.logging.joinleave.guild) || await bot.guilds.fetch(config.logging.joinleave.guild).catch(() => { }),
