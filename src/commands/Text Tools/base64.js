079804234cdd554a7cd5c71eacc9a75b8b8cbdc5
Jarno <jarnostoffer@gmail.com>
2021-07-08 11:54:41 +0200

    style(commands): :sparkles: Fix code styling and add commenting

--- src/commands/Text Tools/base64.js
+++ src/commands/Text Tools/base64.js
@@ -35,26 +35,33 @@ module.exports = {
 
     run: async (bot, message, args) => {
         if (args[0].toLowerCase() === "encode") {
+            // Get the string and encode it in base64
             const string = args.splice(1).join(" "),
-                encoded = Buffer.from(string).toString('base64');
+            encoded = Buffer.from(string).toString('base64');
             
+            // Build the embed
             const embed = new MessageEmbed()
                 .setAuthor("Base64 encoded string", "https://cdn.discordapp.com/emojis/774154612139622410.gif?v=1")
                 .setColor(message.member?.displayColor ?? bot.config.general.embedColor)
                 .setDescription(`\`\`\`${encoded}\`\`\``);
             
+            // Reply with the encoded string
             message.reply({ embeds: [embed] });
         } else if (args[0].toLowerCase() === "decode") {
+            // Get the string and decode it from base64
             const string = args[1],
-                encoded = Buffer.from(string, 'base64').toString();
+            encoded = Buffer.from(string, 'base64').toString();
             
+            // Build the embed
             const embed = new MessageEmbed()
                 .setAuthor("Base64 decoded string", "https://cdn.discordapp.com/emojis/774154612139622410.gif?v=1")
                 .setColor(message.member?.displayColor ?? bot.config.general.embedColor)
                 .setDescription(`\`\`\`${encoded}\`\`\``);
             
+            // Reply with the decoded string
             message.reply({ embeds: [embed] });
         } else {
+            // Send an error
             message.errorReply("You must specify either `encode` or `decode`");
         }
     }
