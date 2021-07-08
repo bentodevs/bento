0152d8f77caa126922a4323ba9b72403c33309ae
Jarno <jarnostoffer@gmail.com>
2021-07-08 11:07:08 +0200

    refactor(commands): :recycle: Update category

--- src/commands/Utility/privacy.js
+++ src/commands/Utility/privacy.js
@@ -12,7 +12,7 @@ module.exports = {
             "privacy all"
         ],
         description: "Allows you to instantly delete your data from R2-D2's database.",
-        category: "Miscellaneous",
+        category: "Utility",
         info: null,
         options: [
             "`reminders` - Clear all your current reminders from the bot's database",
