321d6a70beb53a678b36bca23515ffe7b21805fb
Jarno <jarnostoffer@gmail.com>
2021-07-09 13:46:16 +0200

    feat(cmds): :sparkles: Add wordcount command

---
+++ src/commands/Text Tools/wordcount.js
@@ -0,0 +1,46 @@
+module.exports = {
+    info: {
+        name: "wordcount",
+        aliases: [],
+        usage: "wordcount <text>",
+        examples: [
+            "wordcount this is a test"
+        ],
+        description: "Counts the words and characters in a text.",
+        category: "Text Tools",
+        info: null,
+        options: []
+    },
+    perms: {
+        permission: ["@everyone"],
+        type: "role",
+        self: []
+    },
+    opts: {
+        guildOnly: false,
+        devOnly: false,
+        premium: false,
+        noArgsHelp: true,
+        disabled: false
+    },
+    slash: {
+        enabled: true,
+        opts: [{
+            name: "text",
+            type: "STRING",
+            description: "The text you want to be counted.",
+            required: true
+        }]
+    },
+
+    run: async (bot, message, args) => {
+
+        // If there are no args get them from the interaction options
+        if (!args) 
+            args = message.options.get("text").value.split(/ +/g);
+
+        // Send the reversed text
+        message.reply(`${bot.config.emojis.confirmation} The text you provided has **${args.length}** words and **${args.join("").length}** characters!`);
+
+    }
+};
