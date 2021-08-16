8adecb3e35e0e566018eafc11a85adcd0d426962
Jarno <jarnostoffer@gmail.com>
2021-08-16 14:48:18 +0200

    fix(events): :bug: Add null check

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -14,7 +14,11 @@ module.exports = async (bot, guild) => {
 
     // Get the joins guild and channel
     const g = bot.guilds.cache.get(config.logging.joinleave.guild) || await bot.guilds.fetch(config.logging.joinleave.guild).catch(() => { }),
-        channel = g?.channels.cache.get(config.logging.joinleave.channel);
+    channel = g?.channels?.cache.get(config.logging.joinleave.channel);
+
+    // If the guild or channel weren't found return an error
+    if (!g || !channel)
+        return;
 
     // Send guild joined message
     channel.send(`:wave: Joined guild \`${guild.name}\` (\`${guild.id}\`)`);
