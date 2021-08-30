54d995079dbbc90804b22c63bc47c5b14583ecaa
Behn <hello@behn.cc>
2021-08-30 13:39:04 +0100

    refactor(commands): ♻️ Change Lyrics API to R2-D2

--- src/commands/Miscellaneous/lyrics.js
+++ src/commands/Miscellaneous/lyrics.js
@@ -23,7 +23,7 @@ module.exports = {
         guildOnly: false,
         devOnly: false,
         premium: false,
-        noArgsHelp: true,
+        noArgsHelp: false,
         disabled: false
     },
     slash: {
@@ -33,58 +33,77 @@ module.exports = {
 
     run: async (bot, message, args) => {
 
-        const spotifyData = message.member.presence.activities.find(a => a.name.toLowerCase() == "spotify");
-        
+        // 1. Fetch the user's Spotify presence data
+        // 2. Define the request headers
+        const spotifyData = message.member.presence.activities.find(a => a.name.toLowerCase() == "spotify"),
+            reqHeads = { headers: { "authorization": bot.config.apiKeys.r2d2 } };
+
         if (spotifyData && !args[0]) {
 
-            const req = await fetch(`https://some-random-api.ml/lyrics?title=${spotifyData.details.replace(" ", "+")}+${spotifyData.state.replace(" ", "+")}`),
-            res = await req.json();
+            // 1. Get the song from the API
+            // 2. Convert the data to JSON
+            const req = await fetch(`http://localhost:2021/lyrics/${encodeURIComponent(spotifyData.details)}%20${encodeURIComponent(spotifyData.state)}`, reqHeads),
+                res = await req.json();
 
+            // If there is no lyrics element, then return an error
             if (!res?.lyrics)
                 return message.errorReply("I encountered an error fetching lyics for your current song!");
-            
-            const content = Util.splitMessage(res.lyrics, { maxLength: "1800", char: "\n" });
 
+            // Split the content to be a max of 3800 chars (API limit is 4k chars)
+            const content = Util.splitMessage(res.lyrics, { maxLength: "3800", char: "\n" });
 
+            // For each element int he content array, send an embed
             for (const data of content) {
+                // Build the embed
                 const embed = new MessageEmbed()
-                    .setAuthor(`${res.title} by ${res.author}`, res.thumbnail?.genius ?? bot.user.displayAvatarURL({dynamic: true, format: "png"}))
-                    .setColor(message.member?.displayColor || bot.config.general.embedColor)
+                    .setAuthor(`${res.title}`, res.image ?? bot.user.displayAvatarURL({dynamic: true, format: "png"}))
+                    .setColor(message.member?.displayColor ?? bot.config.general.embedColor)
                     .setDescription(`\`\`\`${data}\`\`\``)
                     .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true, format: "png" }))
                     .setTimestamp();
 
                 if (data == content[0]) {
+                    // If this is the first element in the array then reply to the message
                     message.reply({ embeds: [embed] });
                 } else {
+                    // If this isn't the first element in the array, then just send the embed
                     message.channel.send({ embeds: [embed] });
                 }
             }
 
         } else if (args[0]) {
-            const req = await fetch(`https://some-random-api.ml/lyrics?title=${args.join("+")}`),
+
+            // 1. Get the song from the API
+            // 2. Conver the data to JSON
+            const req = await fetch(`http://localhost:2021/lyrics/${args.join("%20")}`, reqHeads),
             res = await req.json();
 
+            // If there is no lyrics element, then return an error
             if (!res?.lyrics)
                 return message.errorReply("I encountered an error fetching lyics for your current song!");
-            
-            const content = Util.splitMessage(res.lyrics, { maxLength: "1800", char: `\n` });
+
+            // Split the content to be a max of 3800 chars (API limit is 4k chars)
+            const content = Util.splitMessage(res.lyrics, { maxLength: "3800", char: `\n` });
 
             for (const data of content) {
+                // Build the embed
                 const embed = new MessageEmbed()
-                    .setAuthor(`${res.title} by ${res.author}`, res.thumbnail?.genius ?? bot.user.displayAvatarURL({dynamic: true, format: "png"}))
+                    .setAuthor(`${res.title}`, res.image ?? bot.user.displayAvatarURL({dynamic: true, format: "png"}))
                     .setColor(message.member?.displayColor || bot.config.general.embedColor)
                     .setDescription(`\`\`\`${data}\`\`\``)
                     .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true, format: "png" }))
                     .setTimestamp();
 
                 if (data == content[0]) {
+                    // If this is the first element in the array then reply to the message
                     message.reply({ embeds: [embed] });
                 } else {
+                    // If this isn't the first element in the array, then just send the embed
                     message.channel.send({ embeds: [embed] });
                 }
             }
         } else {
+            // If the user isn't listening to a song, and didn't specify a song, then return an error
             message.errorReply("You must be listening to a song, or provide a song to fetch lyrics for!");
         }
 
