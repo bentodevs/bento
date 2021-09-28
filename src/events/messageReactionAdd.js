75bdaef227588469a7936071338530a432631fa9
Jarno <jarnostoffer@gmail.com>
2021-09-28 10:20:22 +0200

    fix(cmds): :bug: Add reactroles code lol

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -1,4 +1,6 @@
 const giveaways = require("../database/models/giveaways");
+const reactroles = require("../database/models/reactroles");
+const { getReactCooldown } = require("../modules/functions/misc");
 
 module.exports = async (bot, reaction, user) => {
     // If the reaction or user are partial try to fetch them
@@ -19,7 +21,41 @@ module.exports = async (bot, reaction, user) => {
         return;
 
     // Check if the reaction message is a giveaway message
-    const giveaway = await giveaways.findOne({ "guild.message_id": reaction.message.id, active: true });
+    const reactRole = await reactroles.findOne({ message: reaction.message.id }),
+    giveaway = await giveaways.findOne({ "guild.message_id": reaction.message.id, active: true });
+
+    // Handle reactroles
+    if (reactRole) {
+        // Get the emote
+        const emote = reactRole.roles.find(r => r.emoji === reaction.emoji.name) || reactRole.roles.find(r => r.emoji === reaction.emoji.id);
+
+        // If the emote wasn't found return
+        if (!emote)
+            return;
+
+        // Grab the role and the member
+        const role = reaction.message.guild.roles.cache.get(emote.role),
+        member = reaction.message.guild.members.cache.get(user.id);
+
+        // If the role wasn't found or the user is on cooldown return
+        if (!role)
+            return;
+        if (getReactCooldown(user, reaction.message.guild.id))
+            return;
+
+        // If the user has the role remove it, otherwise add it
+        if (member.roles.cache.has(role.id)) {
+            member.roles.remove(role.id, "Reaction Role");
+        } else {
+            member.roles.add(role.id, "Reaction Role");
+        }
+
+        // Remove the users reaction
+        reaction.users.remove(user.id);
+
+        // Return
+        return;
+    }
 
     // If the reaction is a giveaway message add the users reaction to the entries
     if (giveaway) {
