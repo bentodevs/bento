8ad1842854df51faea88403001ae19457e4e204e
Behn <7383025+WaitroseDev@users.noreply.github.com>
2021-12-01 23:14:18 +0000

    fix/events: Fix command duplication

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -9,9 +9,6 @@ module.exports = async (bot, guild) => {
         await new settings({ _id: guild.id }).save();
     }
 
-    // Create the guild-only commands
-    await registerGuild(bot, guild.id);
-
     // Get the joins guild and channel
     const g = bot.guilds.cache.get(config.logging.joinleave.guild) || await bot.guilds.fetch(config.logging.joinleave.guild).catch(() => { });
     const channel = g?.channels?.cache.get(config.logging.joinleave.channel);
