b98d239027a03b3fae89373c793a8d690d7cc999
Behn <7383025+WaitroseDev@users.noreply.github.com>
2021-12-09 17:49:11 +0000

    chore/commands: Remove test message

--- src/commands/Information/ping.js
+++ src/commands/Information/ping.js
@@ -30,7 +30,5 @@ export default {
         // Send a message, once the message is sent edit it with the ping information
         message.reply('Never gonna give you up, never gonna let you down...')
             .then((msg) => msg.edit(`:ping_pong: **Time:** ${Math.round(msg.createdTimestamp - message.createdTimestamp)} ms\n:heart: **Heartbeat:** ${Math.round(bot.ws.ping)}ms`));
-
-        message.channel.send("lolxd");
     },
 };
