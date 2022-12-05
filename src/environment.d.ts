f980d183a20c331fe21d456cc680037c21d91b73
Behn <7383025+BehnH@users.noreply.github.com>
2022-12-05 20:30:24 +0000

    Use Grafana Loki for logging

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,17 +1,24 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
+            // Discord Token
             DISCORD_TOKEN: string
-            LAUNCHDARKLY_TOKEN: string
+            // API Keys
             WEATHER_TOKEN: string
             STEAM_TOKEN: string
             RIOT_TOKEN: string
             TRACKER_NETWORK_TOKEN: string
             OMDB_TOKEN: string
             LASTFM_TOKEN: string
-            BENTO_API_TOKEN: string
+            // HTTP Proxy
             WEBPROXY_HOST: string
+            // MongoDB Connection String
             MONGODB_URI: string
+            // Bento Bot API
+            BENTO_API_TOKEN: string
+            // Loki logging API
+            LOKI_URL: string
+            LOKI_AUTH: string
         }
     }
 }
